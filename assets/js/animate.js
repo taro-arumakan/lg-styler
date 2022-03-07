b77bfbd75c964c256eabae55d0c03fb2b8b90b3a
taro <taro.arumakan@gmail.com>
2022-03-07 16:02:29 +0900

    fade sticky profile section past footer

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -5,10 +5,27 @@ function reveal() {
   reveals.forEach(reveal => {
     var elementTop = reveal.getBoundingClientRect().top;
     if (elementTop < windowHeight - elementVisible) {
-      reveal.classList.add('active');
+      if (![...reveal.classList].includes('hidden')) {
+        reveal.classList.add('active');
+      }
     }
   });
 }
-
 window.addEventListener("scroll", reveal);
 window.onload = reveal;
+
+function hide_profile() {
+  var more_element = document.querySelector('.article-footer');
+  var hide_elements = document.querySelectorAll('.hide_past_footer');
+  hide_elements.forEach(he => {
+    if (document.documentElement.scrollTop > more_element.offsetTop){
+      if (![...he.classList].includes('hidden')) {
+        he.classList.remove('active');
+        he.classList.add('hidden');
+      }
+    } else {
+      he.classList.remove('hidden');
+      he.classList.add('active');
+    }
+  })
+}
