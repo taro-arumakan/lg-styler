4c467281bb7978ce7ffd196bbd265c0fc6935c24
taro <taro.arumakan@gmail.com>
2022-04-16 21:22:15 +0900

    ready to split and fade-in texts

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,12 +1,14 @@
 function reveal_init() {
   reveal(0);
+  split_text_to_characters();
 }
 function reveal_scroll() {
   reveal(80);
 }
 function reveal(offset) {
+  var target_selector = offset === 0 ? "[class*=reveal_tran]:not(.reveal_fadein_letter)" : "[class*=reveal]";
   var windowHeight = window.innerHeight;
-  var reveals = document.querySelectorAll("[class*=reveal]");
+  var reveals = document.querySelectorAll(target_selector);
   reveals.forEach(reveal => {
     var elementTop = reveal.getBoundingClientRect().top;
     if (elementTop < windowHeight - offset) {
@@ -16,8 +18,6 @@ function reveal(offset) {
     }
   });
 }
-window.addEventListener("scroll", reveal_scroll);
-window.onload = reveal_init;
 
 function hide_profile() {
   var more_element = document.querySelector('.article-footer');
@@ -52,3 +52,27 @@ function load_file(filename, callback) {
   fetch(filename).then(response => response.text()).then(text => callback(text));
 }
 
+function split_to_spans(n) {
+    chars = n.textContent.replace(/\s+/g, "_").split("");
+    spans = chars.map(s => "_" === s ? " " : `<span>${s}</span>`);
+    spans = spans.map((s, i) => add_delay_style(s, i));
+    return spans;
+}
+function add_delay_style(span, sequence) {
+  if (span.match(/<span>/g)) {
+    var ds = sequence * 3.2;
+    o = 0 === ds ? "" : ` style="transition-delay: ${ds}ms;"`;
+    span = span.replace(/<span>/, `<span${o}>`);
+  }
+  return span;
+}
+function split_text_to_characters() {
+  let to_be_split = document.querySelectorAll('.text-split');
+  [...to_be_split].forEach(ts_node => {
+    spanned = split_to_spans(ts_node);
+    ts_node.innerHTML = "".concat(...spanned);
+  });
+}
+
+window.addEventListener("scroll", reveal_scroll);
+window.onload = reveal_init;
